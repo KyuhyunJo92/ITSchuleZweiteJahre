f5682d9664ca7974e685497b6c388cffeb4f9956
KyuhyunJo92 <52312016+KyuhyunJo92@users.noreply.github.com>
2022-09-13 14:13:13 +0200

    Add files via upload

---
+++ Aufgabe_13.09.22/AZ/AZ1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Aufgabe4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NummerAndereRichtung(12345678));
+        }
+        static int NummerAndereRichtung(int zahl)
+        {
+            int result = 0;
+            
+            string strZahl = Convert.ToString(zahl);
+            StringBuilder sb = new StringBuilder();
+            string revZahl = string.Empty;
+            Stack<char> stackStr= new Stack<char>();
+            foreach(char el in strZahl)
+            {
+                stackStr.Push(el);   
+            }
+            while(stackStr.Count !=0)
+            {
+                sb.Append(stackStr.Pop());
+            }
+            revZahl = sb.ToString();
+            result = Convert.ToInt32(revZahl);
+            return result;
+        }
+    }
+}
