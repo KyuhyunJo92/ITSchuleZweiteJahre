3e1c8f79c1084899f4827fbf5b707eadddd16bad
KyuhyunJo92 <52312016+KyuhyunJo92@users.noreply.github.com>
2022-09-13 14:12:27 +0200

    Add files via upload

---
+++ Aufgabe_13.09.22/AZ/AZ2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace A1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //zweimalBisTousand();
+            LeibnizSchleifer(1000);
+        }
+        static void zweimalBisTousand()
+        {
+            
+            int ergebnis = 1;
+            while(ergebnis < 1000)
+            {
+                ergebnis += ergebnis * 2;
+                Console.WriteLine(ergebnis);
+            }
+        }
+        static void LeibnizSchleifer(int wieVielMal)
+        {
+            double ergebnis = 0.0d;
+            int nenner = 0;
+            for (int n = 0; n<=wieVielMal; n++)
+            {
+                nenner = 1 + n * 2;
+                if (n % 2 == 0)
+                {
+                    //+
+                    ergebnis += 1/Convert.ToDouble(nenner);
+                }
+                else
+                {
+                    ergebnis -= 1/Convert.ToDouble(nenner);
+                    //-
+                }
+            }
+            ergebnis = ergebnis * 4;
+            Console.WriteLine(ergebnis);
+        }
+    }
+}
