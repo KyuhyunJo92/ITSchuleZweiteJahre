ebcfbc107770cdf3501ab86967a976715a4f551c
KyuhyunJo92 <52312016+KyuhyunJo92@users.noreply.github.com>
2022-10-04 00:47:33 +0200

    Add files via upload

---
+++ GeometrischeFiguren/GeometrischenFiguren.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace geometrische_Figuren
+{
+    interface IGeometrischeFigur
+    {
+        double BerechneFlaecheninhalt();
+        string NamenDesFigurUndInhalten();
+        string GetArtDesFigur();
+    }
+
+
+    class Rechteck : IGeometrischeFigur
+    {
+        double breite;
+        double laenge;
+
+        //konstrukt
+        public Rechteck(double Breite, double Laenge) { breite = Breite; laenge = Laenge; }
+
+        public double BerechneFlaecheninhalt()
+        {
+            double flaechen = Math.Round(breite * laenge * 0.5,5);
+            return flaechen;
+        }
+
+        public void SetNewBreite(double NewBreite) { breite = NewBreite; }
+        public void SetNewLaenge(double NewLaenge) { laenge = NewLaenge; }
+        public string NamenDesFigurUndInhalten()
+        {
+            string str;
+            string sBreite = Convert.ToString(breite);
+            string sLaenge = Convert.ToString(laenge);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Rechteck({0},{1})", sBreite, sLaenge);
+            str = sb.ToString();
+            return str;
+        }
+        public string GetArtDesFigur() { return "Rechteck"; }
+    }
+
+
+
+    class Quadrat : IGeometrischeFigur
+    {
+        double seitenLaenge;
+
+        //konstrukt
+        public Quadrat(double SeitenLaenge) { seitenLaenge = SeitenLaenge; }
+
+        public double BerechneFlaecheninhalt()
+        {
+            double flaechen = Math.Round(seitenLaenge * seitenLaenge,5);
+            return flaechen;
+        }
+
+        public void SetNewSeitenLaenge(double NewSeitenLaenge) { seitenLaenge = NewSeitenLaenge; }
+
+        public string NamenDesFigurUndInhalten()
+        {
+            string str;
+            string sSeitenLaenge = Convert.ToString(seitenLaenge);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Quadrat({0})", sSeitenLaenge);
+            str = sb.ToString();
+            return str;
+        }
+        public string GetArtDesFigur() { return "Quadrat"; }
+
+    }
+
+
+
+    class Kreis : IGeometrischeFigur
+    {
+        double radius;
+
+        //konstrukt
+        public Kreis(double Radius) { radius = Radius; }
+
+        public double BerechneFlaecheninhalt()
+        {
+            double flaechen = Math.Round(radius * Constants.pi,5);
+            return flaechen;
+        }
+        public void SetNewRadius(double NewRadius) { radius = NewRadius; }
+        public string NamenDesFigurUndInhalten()
+        {
+            string str;
+            string sRadius = Convert.ToString(radius);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Kreis({0})", sRadius);
+            str = sb.ToString();
+            return str;
+        }
+        public string GetArtDesFigur() { return "Kreis"; }
+    }
+}
---
+++ GeometrischeFiguren/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace geometrische_Figuren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestGeoFigur test = new TestGeoFigur();
+            test.erstellenRandomKreise(5);
+            test.erstellenRandomQuadrat(5);
+            test.erstellenRandomRechteck(5);
+
+            test.SehenAllenDetailDesObjekts(test.GetKreisen());
+            test.SehenAllenFlaecheninhaltDesFigurList(test.GetKreisen());
+            
+            test.SehenAllenDetailDesObjekts(test.GetQuadrats());
+            test.SehenAllenFlaecheninhaltDesFigurList(test.GetQuadrats());
+            
+            test.SehenAllenDetailDesObjekts(test.GetRechtecks());
+            test.SehenAllenFlaecheninhaltDesFigurList(test.GetRechtecks());
+        }
+    }
+
+    static class Constants
+    {
+        public const double pi = 3.141592;
+    }
+
+    
+    class TestGeoFigur
+    {
+        //Der Typ von List muss List fuer IGeometrischeFigur.
+        //z.B List<Kreis> Kreisen funktiniert nicht.
+
+        private List<IGeometrischeFigur> Rechtecks;
+        private List<IGeometrischeFigur> Quadrats;
+        private List<IGeometrischeFigur> Kreisen;
+        private Random rand = new Random();
+        public List<IGeometrischeFigur> GetKreisen() { return Kreisen; }
+        public List<IGeometrischeFigur> GetRechtecks() { return Rechtecks; }
+        public List<IGeometrischeFigur> GetQuadrats() { return Quadrats; }
+        //Konstruktor
+        public TestGeoFigur()
+        {
+            Kreisen = new List<IGeometrischeFigur>();
+            Rechtecks = new List<IGeometrischeFigur>();
+            Quadrats = new List<IGeometrischeFigur>();
+        }
+
+        public void erstellenRandomKreise(int wieVielmal)
+        {
+            for (int i = 0; i < wieVielmal; i++)
+            {
+                Kreis newKreis = new Kreis(Math.Round(rand.NextDouble()*10, 2));
+                Kreisen.Add(newKreis);
+            }
+        }
+        public void erstellenRandomRechteck(int wieVielmal)
+        {
+            for (int i = 0; i < wieVielmal; i++)
+            {
+                Rechteck newRechteck = new Rechteck(Math.Round(rand.NextDouble() * 10, 2), Math.Round(rand.NextDouble() * 10, 2));
+                Rechtecks.Add(newRechteck);
+            }
+        }
+        public void erstellenRandomQuadrat(int wieVielmal)
+        {
+            for (int i = 0; i < wieVielmal; i++)
+            {
+                Quadrat newQuadrat = new Quadrat(Math.Round(rand.NextDouble() * 10, 2));
+                Quadrats.Add(newQuadrat);
+            }
+        }
+        
+        public void SehenAllenFlaecheninhaltDesFigurList(List<IGeometrischeFigur> geometrischesFiguren)
+        {
+            int counter = 0;
+            foreach (IGeometrischeFigur geoFigur in geometrischesFiguren)
+            {
+                Console.WriteLine("{0}te {1}'s Flaecheninhalt : {2}",
+                    counter,
+                    geoFigur.GetArtDesFigur(),
+                    Convert.ToString(geoFigur.BerechneFlaecheninhalt()));
+                counter++;
+            }
+        }
+
+        public void SehenAllenDetailDesObjekts(List<IGeometrischeFigur> geometrischesFiguren)
+        {
+            foreach (IGeometrischeFigur geoFigur in geometrischesFiguren)
+            {
+                Console.WriteLine(geoFigur.NamenDesFigurUndInhalten());
+            }
+        }
+    }
+}
